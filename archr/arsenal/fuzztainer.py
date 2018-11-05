1f54b6ffe107d5890b5d3c291674a9dafddedb71
haehyun <haehyuncho@gmail.com>
2018-11-05 04:42:19 +0000

    preparing fuzztainer arrow and bow

---
+++ archr/arsenal/fuzztainer.py
@@ -0,0 +1,16 @@
+import logging
+
+l = logging.getLogger("archr.arsenal.fuzztainer")
+
+from . import Bow
+
+class FuzzTainerBow(Bow):
+    """
+    Fuzzing a target in the form of a Docker image.
+    """
+
+    def __init__(self, target):
+
+    def nock(self):
+
+    def fire(self):
